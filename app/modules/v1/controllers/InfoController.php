b0878a3c535ac4ea606f8fb0aa77ca7ab563eaa6
Your Name <you@example.com>
2020-12-20 22:35:49 +0500

    remove unused code

--- app/modules/v1/controllers/InfoController.php
+++ app/modules/v1/controllers/InfoController.php
@@ -3,25 +3,11 @@
 namespace app\modules\v1\controllers;
 
 use app\controllers\BaseApiController;
-use yii\filters\VerbFilter;
 use app\modules\v1\models\ClientInfo;
 use yii\web\BadRequestHttpException;
 
 class InfoController extends BaseApiController
 {
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        return array_merge($behaviors, [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'total-volume' => ['get'],
-                ],
-            ],
-        ]);
-    }
-
     public function actionClientInfo()
     {
         $model = new ClientInfo();
